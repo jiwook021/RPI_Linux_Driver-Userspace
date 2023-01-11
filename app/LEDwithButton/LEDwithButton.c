24bb0543bc8dc49b7cdaaf5a9904c6d9a1421b23
jiwook021 <jiwook021@gmail.com>
2023-01-10 21:26:21 -0800

    addition of LED with button using mmap. turn on off by button. and if receiveds intterrupt. close fd and munmap

--- app/LEDwithButton/LEDwithButton.c
+++ app/LEDwithButton/LEDwithButton.c
@@ -5,17 +5,18 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/mman.h>
-
+#include <signal.h>
 //#define GPIO_BASE 0x3F200000
 #define GPIO_BASE 0x00200000
-#define GPFSEL1 0x04
-#define GPSET0 0x1C
-#define GPCLR0 0x28
+// #define GPFSEL1 0x04
+// #define GPSET0 0x1C
+// #define GPCLR0 0x28
 
 int fd;
 char *gpio_memory_map;
 volatile unsigned int* gpio;
 
+void  INThandler(int);
 
 int main()
 {
@@ -42,28 +43,44 @@ int main()
     }
 
     volatile unsigned int* gpio = (volatile unsigned int*)gpio_memory_map;
-    volatile unsigned int* gpio_direction_GPFSEL1 = gpio + GPFSEL1/4;
-    volatile unsigned int* gpio_set_GPSET0 = gpio + GPSET0/4;
-    volatile unsigned int* gpio_set_GPCLR0 = gpio + GPCLR0/4;
+    // volatile unsigned int* gpio_direction_GPFSEL1 = gpio + 0x04/4;
+    // volatile unsigned int* gpio_set_GPSET0 = gpio + 0x1C/4;
+    // volatile unsigned int* gpio_set_GPCLR0 = gpio + 0x28/4;
 
-    *gpio_direction_GPFSEL1 &= ~(0x03 << 24); 
-    *gpio_direction_GPFSEL1 |= 0x01 << 24; 
+    *(gpio + 0x04/4) &= ~(0x03 << 24); //clear pin 18 to 00
+    *((gpio + 0x04/4)) |= 0x01 << 24; //set for pin 18 to set output
 
-    int i;
 
-    for ( i=0; i<5; i++ )
+    *(gpio + 0x04/4) &= ~(0x03 << 21); //clear for pin 17 to 00
+    *((gpio + 0x04/4)) |= 0x00 << 21; //set for pin 17 to set input
+
+
+    signal(SIGINT, INThandler);
+
+    while(1)
     {
-        *gpio_set_GPSET0 |= (1<<18);
-        printf("gpio18(LED) On \n");
-        usleep (1000000);
 
-        *gpio_set_GPCLR0 |= (1<<18);
-        printf("gpio18(LED) Off \n");
-        usleep (1000000);
+        int in = *(gpio + 0X34/4);
+        if(in & (0x01 << 17)) //if high
+        {
+            *(gpio + 0x1C/4) |= (1<<18); //set
+        }
+        else
+        {
+            *(gpio + 0x28/4) |= (1<<18);   //clr    
+        }
     }
-
     munmap( gpio_memory_map, 4096);
     close(fd);
 
     return 0;
+}
+
+void INThandler(int sig)
+{
+    printf("\ninterrupt by %d",sig);
+    munmap( gpio_memory_map, 4096);
+    close(fd);
+    printf("\nclosed fd and memory unmammped. \nend the process by exit(0)\n");
+    exit(0);
 }
